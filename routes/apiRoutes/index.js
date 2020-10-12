0a4db185a5ee3cabb7b4ed33e506c595dedf67ad
luc1dLife <luc1d_life@protonmail.com>
2020-10-12 02:26:20 -0600

    Completed App

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const animalRoutes = require("../apiRoutes/animalRoutes");
 
+router.use(require("./zookeeperRoutes"));
 router.use(animalRoutes);
 
 module.exports = router;
