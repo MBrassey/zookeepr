a6a486c7efed8ce85fdcc97526a52ce0206c7997
luc1dLife <luc1d_life@protonmail.com>
2020-10-11 23:02:18 -0600

    +Front End

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const path = require("path");
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+// Make css / js / image files available
+app.use(express.static("public"));
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -113,6 +115,18 @@ app.post("/api/animals", (req, res) => {
     const animal = createNewAnimal(req.body, animals);
     res.json(animal);
   }
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/animals", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/animals.html"));
+});
+
+app.get("/zookeepers", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
 });
 
 app.listen(PORT, () => {
