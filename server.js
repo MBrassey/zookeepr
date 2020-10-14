becfea86244936ee303c687a8802ebf787dd043f
luc1dLife <luc1d_life@protonmail.com>
2020-10-13 18:59:29 -0600

    Cleaned

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require("express");
-const { animals } = require("./data/animals.json");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const fs = require("fs");
-const path = require("path");
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
 
